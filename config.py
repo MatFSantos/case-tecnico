f12f2e146b530337e5216dfa7198f45b52b7e921
Mateus Fernandes Santos <mateus.santos@beegol.com>
2024-05-08 11:33:14 -0300

    add color configuration

---
+++ config.py
@@ -0,0 +1,19 @@
+COLORS = {
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+    'white': '\033[37m',
+    'black_bg': '\033[40m',
+    'red_bg': '\033[41m',
+    'green_bg': '\033[42m',
+    'yellow_bg': '\033[43m',
+    'blue_bg': '\033[44m',
+    'magenta_bg': '\033[45m',
+    'cyan_bg': '\033[46m',
+    'white_bg': '\033[47m',
+    'reset': '\033[0m'
+}
